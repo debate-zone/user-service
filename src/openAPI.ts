e4bb2c33fcb0002ce343a7a3b88802e8307830b4
Grigori Fiodorov <grigori@q-ueue.ai>
2023-04-16 23:30:54 +0300

    added log for inserting new user added openAPI config added version from package json for api added dotenv

---
+++ src/openAPI.ts
@@ -0,0 +1,14 @@
+import { OpenAPI } from "express-zod-api";
+import {routing} from "./route";
+import {config} from "./config";
+import manifest from "../package.json";
+import 'dotenv/config'
+
+const specAsYaml = new OpenAPI({
+    routing,
+    config,
+    version: manifest.version,
+    title: "User API",
+    serverUrl: process.env.SERVER_URL!,
+    composition: "components",
+}).getSpecAsYaml()
