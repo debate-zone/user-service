8766fbccc678e8091b8c74852b916b53dc74ec52
Grigori Fiodorov <grigori.fiodorov@gmail.com>
2023-04-16 23:30:54 +0300

    added log for inserting new user added openAPI config added version from package json for api added dotenv

--- src/endpoint.ts
+++ src/endpoint.ts
@@ -3,13 +3,18 @@ import {newUserSchema, outputSchema} from "./zodSchema"
 import {userDbController} from "./dbController";
 
 export const newUser = defaultEndpointsFactory.build({
+    shortDescription: "Create a new user",
+    description: "Create a new user",
     method: "post",
     input: newUserSchema,
     output: outputSchema,
     handler: async ({ input, options, logger }): Promise<{}> => {
-        logger.debug("Options:", options)
+        logger.info("Creating new user", input)
 
-        await userDbController.save(input)
+        await userDbController.save({
+            email: input.email
+        }, input)
+
         return {}
     }
 })
